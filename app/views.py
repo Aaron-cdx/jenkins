e9ed4eb4349835881c9cd2d3a5366ea488b07ecb
caoduanxi <1315020626@qq.com>
2021-05-29 16:19:24 +0800

    modifiy the file

--- app/views.py
+++ app/views.py
@@ -7,4 +7,5 @@ def index(request):
     # return HttpResponse("hello welcome to the django! Try hard study to Jenkins!")
     # message = "Hello world~"
     message = "Hello world \n 本次是自动部署的，不含有任何的人工成分"
-    return render(request, "index.html", {"message": message})
+    message2 = "再增加一条测试记录"
+    return render(request, "index.html", {"message": message, "message2": message2})
