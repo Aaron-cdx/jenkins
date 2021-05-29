e346c9665c1cffcb4dc9b47e0572bf0c792a8386
caoduanxi <1315020626@qq.com>
2021-05-29 15:32:34 +0800

    modifiy the setting debug->false“

--- jenkins/settings.py
+++ jenkins/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)sp4*0j5djx+61ei$!+%hwq%+^a7lp3v%!m3)azgbz7c4t+^ye'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
