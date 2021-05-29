a95df5e782cc090a8c3d14a57cd79805e62b35af
caoduanxi <1315020626@qq.com>
2021-05-29 15:35:37 +0800

    change the message content

--- app/views.py
+++ app/views.py
@@ -5,5 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     # return HttpResponse("hello welcome to the django! Try hard study to Jenkins!")
-    message = "Hello world~"
+    # message = "Hello world~"
+    message = "Hello world \n 本次是自动部署的，不含有任何的人工成分"
     return render(request, "index.html", {"message": message})
